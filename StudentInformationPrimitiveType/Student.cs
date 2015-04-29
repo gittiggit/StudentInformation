a724cd203b4891ac263ba7c16e52fb91a9bf3eee
gittiggit <contactwithmahmud@gmail.com>
2015-04-29 20:29:35 +0600

    Second time comitted

---
+++ StudentInformationPrimitiveType/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentInformationPrimitiveType
+{
+    class Student
+    {
+        public string regNo;
+        public string firstName;
+        public string lastName;
+
+        public string GetFullName()
+        {
+            return firstName + " " + lastName;
+        }
+
+        public string GetCode(string code)
+        {
+            return regNo + code;
+        }
+    }
+}
---
+++ StudentInformationPrimitiveType/StudentInformationUI.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace StudentInformationPrimitiveType
+{
+    public partial class StudentInformationUI : Form
+    {
+        public StudentInformationUI()
+        {
+            InitializeComponent();
+        }
+      
+        Student aStudent = new Student();
+        //private string regNo;
+        //private string firstName;
+        //private string lastName;
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+           
+            aStudent.regNo = regNoTextBox.Text;
+            aStudent.firstName = firstNameTextBox.Text;
+            aStudent.lastName = lastNameTextBox.Text;
+            ClearStudentInformationFromTextBoxes();
+            MessageBox.Show(aStudent.GetFullName() + @", your registration number is " + aStudent.regNo+ "\nYour code no is "+aStudent.GetCode("123"));
+        }
+
+        private void ClearStudentInformationFromTextBoxes()
+        {
+            regNoTextBox.Text = "";
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+        }
+
+        private void retriveButton_Click(object sender, EventArgs e)
+        {
+            regNoTextBox.Text = aStudent.regNo;
+            firstNameTextBox.Text = aStudent.firstName;
+            lastNameTextBox.Text = aStudent.lastName;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+          
+            aStudent= new Student();
+            //aStudent.regNo = "";
+            //aStudent.firstName = "";
+            //aStudent.lastName = "";
+            ClearStudentInformationFromTextBoxes();
+        }
+    }
+}
